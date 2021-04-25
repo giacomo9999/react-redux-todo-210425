39159af3526cb60c708d4577dc07c847946d5a5c
giacomo9999 <gmarchesi888@gmail.com>
2021-04-25 13:42:04 -0400

    Fixed typo in reducer.

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,14 +7,11 @@ const initialState = {
 const appReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FRUIT:
-      console.log("Reducer adding fruit...", action.fruit);
-      console.log("Reducer current state...", state.fruits);
       return {
         ...state,
         fruits: state.fruits.concat(action.fruit),
       };
     case DELETE_FRUIT:
-      console.log("Reducer deleting fruit...", action.fruit);
       const filteredFruits = state.fruits.filter((fruit) => {
         return fruit !== action.fruit;
       });
